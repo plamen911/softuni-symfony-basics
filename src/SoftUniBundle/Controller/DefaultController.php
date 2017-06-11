dda85d3fe6e507e7ff460635b2333f769a712a93
apple <1>
2017-06-11 21:26:38 +0300

    Create new bundle

---
+++ src/SoftUniBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SoftUniBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('SoftUniBundle:Default:index.html.twig');
+    }
+}
---
+++ src/SoftUniBundle/SoftUniBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SoftUniBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SoftUniBundle extends Bundle
+{
+}
